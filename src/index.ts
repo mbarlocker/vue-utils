545dff7fac0b26f0a0b9423fbcc84d5f15121a3e
Matthew Barlocker <mbarlocker@gmail.com>
2024-08-06 20:04:45 -0600

    router-coalesce lib

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as focus from './focus/index.js'
 import * as jwt from './jwt/index.js'
 import * as lazy from './lazy/index.js'
+import * as routerCoalesce from './router-coalesce/index.js'
 import * as script from './script/index.js'
 import * as timezone from './timezone/index.js'
 import * as titleParts from './title-parts/index.js'
@@ -8,6 +9,7 @@ import * as titleParts from './title-parts/index.js'
 export { focus as focus }
 export { jwt as jwt }
 export { lazy as lazy }
+export { routerCoalesce as routerCoalesce }
 export { script as script }
 export { timezone as timezone }
 export { titleParts as titleParts }
@@ -16,6 +18,7 @@ export default {
 	focus,
 	jwt,
 	lazy,
+	routerCoalesce,
 	script,
 	timezone,
 	titleParts,
---
+++ src/router-coalesce/index.ts
@@ -0,0 +1,32 @@
+import { isArray } from 'lodash'
+
+export const coalesceSingle = <T>(value: T | Array<T | null> | null): undefined | T => {
+	return coalesceMultiple(value)?.[0]
+}
+
+export const coalesceMultiple = <T>(value: T | Array<T | null> | null): undefined | T[] => {
+	if (value === null || value === undefined) {
+		return undefined
+	}
+
+	const ret: T[] = []
+	if (isArray(value)) {
+		value.forEach(v => {
+			if (v !== null && v !== undefined) {
+				ret.push(v)
+			}
+		})
+	}
+	else {
+		ret.push(value)
+	}
+
+	return ret
+}
+
+export const coalesce = coalesceSingle
+
+export default {
+	coalesceSingle,
+	coalesceMultiple,
+}

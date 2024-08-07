0393ff1546306315a7128abcde1274278e585725
Matthew Barlocker <mbarlocker@gmail.com>
2024-08-06 19:32:38 -0600

    lazy functions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import * as focus from './focus/index.js'
+import * as lazy from './lazy/index.js'
 import * as script from './script/index.js'
 import * as timezone from './timezone/index.js'
 
 export { focus as focus }
+export { lazy as lazy }
 export { script as script }
 export { timezone as timezone }
 
 export default {
 	focus,
+	lazy,
 	script,
 	timezone,
 }
---
+++ src/lazy/index.ts
@@ -0,0 +1,14 @@
+
+export function lazy<T>(fn: () => T): () => T {
+	let result: T | null = null
+
+	return () => {
+		if (result === null) {
+			result = fn()
+		}
+
+		return result
+	}
+}
+
+export default lazy

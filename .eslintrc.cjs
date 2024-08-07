f3a9b7e1c65bab9a78ab115135f2bf8d5e7bed58
Matthew Barlocker <mbarlocker@gmail.com>
2024-08-06 19:16:51 -0600

    eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.esm.json', './tsconfig.cjs.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  root: true,
+}

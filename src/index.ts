8c6a31ffc55200cc0ad9f227b6afcd701f8bb2ac
Matthew Barlocker <mbarlocker@gmail.com>
2024-08-06 19:32:02 -0600

    timezone functions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as focus from './focus/index.js'
 import * as script from './script/index.js'
+import * as timezone from './timezone/index.js'
 
 export { focus as focus }
 export { script as script }
+export { timezone as timezone }
 
 export default {
 	focus,
 	script,
+	timezone,
 }
---
+++ src/timezone/index.ts
@@ -0,0 +1,18 @@
+
+export function currentZone(): string {
+	return Intl.DateTimeFormat().resolvedOptions().timeZone
+}
+
+export function localToUtcDate(date: Date): Date {
+	return new Date(date.getTime() - date.getTimezoneOffset() * 60_000)
+}
+
+export function utcToLocalDate(date: Date): Date {
+	return new Date(date.getTime() + date.getTimezoneOffset() * 60_000)
+}
+
+export default {
+	currentZone,
+	localToUtcDate,
+	utcToLocalDate,
+}

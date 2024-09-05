0a2280b30d7892259da9644baa7fe6e4d7cab055
Matthew Barlocker <mbarlocker@gmail.com>
2024-09-04 20:00:48 -0600

    rework jumper lib to use session storage instead of query/params

--- src/jumper/index.ts
+++ src/jumper/index.ts
@@ -1,57 +1,102 @@
-import { coalesceSingle } from '../router-coalesce/index.js'
 import { isNavigationFailure } from 'vue-router'
+import { RouteLocation } from 'vue-router'
+import { Router } from 'vue-router'
 import { useRouter } from 'vue-router'
+import { useSessionStorage } from '@vueuse/core'
 
-export const useJumper = () => {
+export type JumpReturn = ReturnType<Router['replace']>
+
+export function useJumper(name: string) {
 	const router = useRouter()
+	const current = useSessionStorage<string | undefined>(`jumper-${name}`, undefined)
 
-	const fromQuery = (name: string): string | undefined => {
-		return coalesceSingle(router.currentRoute.value.query[name])
+	const clear = (): void => {
+		current.value = undefined
 	}
 
-	const fromParam = (name: string): string | undefined => {
-		return coalesceSingle(router.currentRoute.value.params[name])
+	const set = (url: string): void => {
+		current.value = url
 	}
 
-	const fromChain = (name: string): string | undefined => {
-		return fromParam(name) ?? fromQuery(name)
-	}
-
-	const keep = (name: string = 'jump') => {
-		const path = fromChain(name)
-		if (path === undefined) {
-			return {}
-		}
-		else {
-			return {
-				[`${name}`]: path,
-			}
-		}
-	}
-
-	const jump = (name: string = 'jump', replace: boolean = false): Promise<any> => {
-		const chain = fromChain(name)
-		const url = decodeURIComponent(chain ?? '')
-
-		if (!url) {
-			return Promise.reject(new Error('No jump available'))
+	const get = (): string => {
+		if (current.value === undefined) {
+			throw new Error('Jumper value is not defined')
 		}
 
-		const canonical = new URL(url, window.location.href)
+		return current.value
+	}
+
+	const getSafe = (): string | undefined => {
+		return current.value
+	}
+
+	const exists = () => {
+		return current.value !== undefined
+	}
+
+	const executeJump = (location: string | RouteLocation, replace: boolean): JumpReturn => {
+		if (typeof location !== 'string') {
+			location = router.resolve(location).path
+		}
+
+		const canonical = new URL(location, window.location.href)
 		if (canonical.pathname === router.currentRoute.value.path) {
 			return Promise.reject(new Error('Refusing to jump to current route'))
 		}
 
 		const fn = replace ? router.replace : router.push
-		return fn(url)
+		return fn(location)
 			.then(failure => {
-				if (isNavigationFailure(failure)) { throw failure }
+				if (isNavigationFailure(failure)) {
+					throw failure
+				}
+
 				return failure
 			})
 	}
 
+	const jump = (replace: boolean = false): JumpReturn => {
+		const url = current.value
+		if (url === undefined) {
+			return Promise.reject(new Error('No jump available'))
+		}
+
+		current.value = undefined
+		return executeJump(url, replace)
+	}
+
+	const jumpOrFirst = (locations: RouteLocation[], replace: boolean = false): JumpReturn => {
+		let index = 0
+
+		function next(lastError: Error): Promise<any> {
+			const location = locations[index++]
+
+			if (location === undefined) {
+				throw lastError
+			}
+
+			return executeJump(location, replace).catch(next)
+		}
+
+		return jump(replace).catch(next)
+	}
+
+	const redirect = (to: RouteLocation, replace: boolean): JumpReturn => {
+		current.value = router.currentRoute.value.path
+
+		const fn = replace ? router.replace : router.push
+		return fn(to)
+	}
+
 	return {
+		clear,
+		current,
+		exists,
+		get,
+		getSafe,
 		jump,
-		keep,
+		jumpOrFirst,
+		redirect,
+		set,
 	}
 }

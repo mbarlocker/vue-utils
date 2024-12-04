94c02ec905e14136b9bff306b2bf0c0970346c8f
Matthew Barlocker <mbarlocker@gmail.com>
2024-12-04 08:46:42 -0700

    bootstrap radio button

--- src/bootstrap-inputs/BootstrapRadioButton.vue
+++ src/bootstrap-inputs/BootstrapRadioButton.vue
@@ -1,45 +1,37 @@
 <template>
-	<div class="btn-group">
-		<BootstrapRadioButton
-			v-for="option in options"
-			:key="option.id"
+	<span>
+		<input
 			v-model="modelValue"
+			type="radio"
+			class="btn-check"
 			:name="name"
-			:value="option.id"
-			:id="`${id}-${option.id}`"
-			:label="option.name"
-			:disabled="option.disabled"
+			:value="value"
+			:id="`${id}`"
+			:disabled="disabled"
 			:required="required"
-			:size="size"
-			:color="color"
 		/>
-	</div>
+
+		<label
+			:class="`btn btn-${size} btn-outline-${color}`"
+			:for="`${id}`"
+		>
+			{{label}}
+		</label>
+	</span>
 </template>
 
 <script lang="ts">
-import BootstrapRadioButton from './BootstrapRadioButton.vue'
-import type { PropType } from 'vue'
-import { computed } from 'vue'
 import { defineComponent } from 'vue'
 import { useVModel } from '../vmodel/index.js'
 
-export interface Option {
-	id: string | number
-	name: string
-	disabled?: boolean
-}
-
 export default defineComponent({
-	components: {
-		BootstrapRadioButton,
-	},
 	props: {
 		modelValue: {
 			type: [String, Number],
 			default: '',
 		},
-		options: {
-			type: Array as PropType<(Option | string)[]>,
+		value: {
+			type: [String, Number],
 			required: true,
 		},
 		name: {
@@ -49,6 +41,10 @@ export default defineComponent({
 		id: {
 			type: String,
 			default: () => `radio-${+new Date}-${Math.random()}`,
+		},
+		label: {
+			type: String,
+			required: true,
 		},
 		size: {
 			type: String,
@@ -62,6 +58,10 @@ export default defineComponent({
 			type: Boolean,
 			default: false,
 		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
 	},
 	emits: [
 		'update:modelValue',
@@ -69,22 +69,8 @@ export default defineComponent({
 	setup: (props, context) => {
 		const modelValue = useVModel(() => `${props.modelValue}`, (value) => context.emit('update:modelValue', value))
 
-		const options = computed<Option[]>(() => {
-			return props.options.map(o => {
-				if (typeof o === 'string') {
-					return {
-						id: o,
-						name: o,
-					}
-				}
-
-				return o
-			})
-		})
-
 		return {
 			modelValue,
-			options,
 		}
 	},
 })
